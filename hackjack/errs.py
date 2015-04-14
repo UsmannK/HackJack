ce77b2ea922c52559daf4599fda6fb8473c72cd2
UsmannK <usmann96@gmail.com>
2015-04-14 09:45:47 -0400

    Lowercased error messages

--- hackjack/errs.py
+++ hackjack/errs.py
@@ -1,3 +1,3 @@
-table_not_found = {"error":"Table not found"}
-wait_your_damn_turn = {"error":"Please wait until your turn"}
-join_the_game = {"error":"You are not in this game"}
+table_not_found = {"error":"table not found"}
+wait_your_damn_turn = {"error":"please wait until your turn"}
+join_the_game = {"error":"you are not in this game"}

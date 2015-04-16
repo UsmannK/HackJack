8dc1d89b89312ab42e2ebcdecd9ecbdb1b3aa348
UsmannK <usmann96@gmail.com>
2015-04-16 17:19:00 -0400

    Merge?

--- tasks.py
+++ tasks.py
@@ -1,18 +1,25 @@
 from celery import Celery
+from flask import Flask
+
+app = Flask(__name__)
+
 
 def make_celery(app):
-    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
-    class ContextTask(TaskBase):
-        abstract = True
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
+	celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+	celery.conf.update(app.config)
+	TaskBase = celery.Task
+	class ContextTask(TaskBase):
+	    abstract = True
+	    def __call__(self, *args, **kwargs):
+	        with app.app_context():
+	            return TaskBase.__call__(self, *args, **kwargs)
+	celery.Task = ContextTask
+	return celery
 
-@app.task
+celery = make_celery(app)
+
+
+@celery.task
 def add(x, y):
     return x + y
 
